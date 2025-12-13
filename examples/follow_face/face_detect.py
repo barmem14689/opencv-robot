7908b96356eabdf5692593c019f892beef36ad8f
george <slslclalal@gmail.com>
2025-12-13 23:50:43 +0300

    Create face_detect.py

---
+++ examples/follow_face/face_detect.py
@@ -0,0 +1,24 @@
+import cv2
+from time import sleep
+import serial
+
+def zov(x,in_min,in_max,out_min,out_max):
+    z = (x-in_min) * (out_max - out_min) / (in_max - in_min) + out_min
+    return int(z)
+
+ser = serial.Serial('COM5',9600,timeout=1)
+cap = cv2.VideoCapture(1)
+
+cascade = cv2.CascadeClassifier('cascades/face.xml')
+
+while True:
+    result,frame = cap.read()
+    if not result:
+        break
+    gray_img = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    faces = cascade.detectMultiScale(gray_img,1.3,5)
+    for(x,y,w,h) in faces:
+        ox = x+w //2
+    cx = zov(ox,155,515,0,255)
+    print(cx)
+    ser.write(bytes([cx]))
